5d80b6df13bd141b7d38bd27902e3426f2a4c7d3
Maitre-Pangolin <martin.wasselet@gmail.com>
2021-08-05 15:38:51 -0400

    Modify app call to the API and render

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
+import Letter from "./Letter";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("/api")
+    fetch("/api/generator")
       .then((res) => res.json())
-      .then(({ message }) => setData(message));
+      .then(({ message }) => setData(Array.from(message)));
   }, []);
 
   return (
     <div className="App">
-      <h1>{!data ? "Loading..." : data}</h1>
+      {/*<h1>{!data ? "Loading..." : data}</h1>
+      <Letter letter="b"></Letter>*/}
+      <div className="letter-container">
+        {data.map((letter, index) => (
+          <Letter letter={letter} key={index} offset={index}></Letter>
+        ))}
+      </div>
     </div>
   );
 }
